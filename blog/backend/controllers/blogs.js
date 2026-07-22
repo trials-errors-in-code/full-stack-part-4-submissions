78b57c6a5872f0ebe5af95a5916f7bc4ce9bb2a7
codeassisttrial-ship-it <119730997+trials-errors-in-code@users.noreply.github.com>
2026-07-22 22:24:01 +0530

    works and runs a bit

--- blog/backend/controllers/blogs.js
+++ blog/backend/controllers/blogs.js
@@ -2,19 +2,30 @@ import express from "express";
 import logger from "../utils/logger.js";
 import Blog from "../models/blog.js";
 
+let count = 1;
 const blogsRouter = express.Router();
-blogsRouter.get('/', (req,res)=>{
-  console.log("Getting api/blogs")
-  Blog.find({}).then(data=>res.json(data))
-})
-blogsRouter.post('/',(req, res) => {
-  const {title, author, url, likes}=req.body
+
+blogsRouter.get("/", (req, res) => {
+  console.log("Getting api/blogs");
+  Blog.find({}).then((data) => {
+    console.log((count++)/2);
+    res.json(data);
+  });
+});
+
+blogsRouter.post("/", (req, res) => {
+  const { title, author, url, likes } = req.body;
   const blog = new Blog({
-    title:title||"none",
-    author:author||"none",
-    url:url||"no url provided",
-    likes:likes
-  })
-  blog.save().then(blog=>res.json(blog)).catch(error=>logger.error("error posting the data to mongo db",error.message))
-})
+    title: title || "none",
+    author: author || "none",
+    url: url || "no url provided",
+    likes: likes,
+  });
+  blog
+    .save()
+    .then((blog) => res.json(blog))
+    .catch((error) =>
+      logger.error("error posting the data to mongo db", error.message),
+    );
+});
 export default blogsRouter;
--- blog/backend/utils/middleware.js
+++ blog/backend/utils/middleware.js
@@ -1,3 +1,24 @@
-const middleware = "Middleware"
+import logger from "./logger.js";
 
-export default middleware
+const requestLogger = (req, res, next) => {
+  logger.info("Method", req.method);
+  logger.info("Path", req.path);
+  logger.info("Body", req.body);
+  logger.info("---");
+  next();
+};
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message);
+  if (error.name === "CastError") {
+    return res.status(400).send("malformatted id");
+  } else if (error.name === "ValidationError") {
+    return res.status(404).json({ error: error.message });
+  }
+  next(error);
+};
+
+const unknownEndpoint = (req, res) => {
+  return res.status(404).send({ error: "unknown endpoint" });
+};
+export default { requestLogger };
