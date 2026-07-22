6ff3a7fab8a20d28cddf5dbbf8cb2111f0e68012
Karan Pratap Singh Parmar <119730997+trials-errors-in-code@users.noreply.github.com>
2026-07-22 16:48:40 +0530

    Update blogs.js

--- blog/backend/controllers/blogs.js
+++ blog/backend/controllers/blogs.js
@@ -1,6 +1,20 @@
-import { Router } from "express";
-import Blog from "../models/blog";
+import express from "express";
+import logger from "../utils/logger.js";
+import Blog from "../models/blog.js";
 
 const blogsRouter = express.Router();
-
+blogsRouter.get('/', (req,res)=>{
+  console.log("Getting api/blogs")
+  Blog.find({}).then(data=>res.json(data))
+})
+blogsRouter.post('/',(req, res) => {
+  const {title, author, url, likes}=req.body
+  const blog = new Blog({
+    title:title||"none",
+    author:author||"none",
+    url:url||"no url provided",
+    likes:likes
+  })
+  blog.save().then(blog=>res.json(blog)).catch(error=>logger.error("error posting the data to mongo db",error.message))
+})
 export default blogsRouter;
